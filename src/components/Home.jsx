6d37ef1126a81b75b771c6121727c9dcc0c57a58
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-08-03 07:09:19 +0530

    added input location

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios';
 
+
 const Home = () => {
     const [weatherData, setWeatherData] = useState(null);
     const apiKey = "9faffac7c31af858ba42b0d533fe8ba4";
-    const query = "New York"; // Replace with the desired location 
+    const [query, setQuery] = useState("New York");// Replace with the desired location 
     useEffect(() => {
         const encodedQuery = encodeURIComponent(query);
         // Fetch weather data from Weatherstack API
@@ -23,12 +24,18 @@ const Home = () => {
                 console.error("Error fetching weather data:", error);
             });
     }, [query]); // Run the effect whenever the 'query' changes
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    };
 
     return (
         <div>
-        
+
             <div className="h-container">
                 <div className="details">
+                    <form>
+                        <input type="text" className='input' value={query} onChange={handleInputChange} placeholder="Enter location"  />
+                    </form>
                     <h1>Real-Time & <br />Historical World<br /> Weather Data </h1>
                     <p>Retrieve instant, accurate weather<br /> information forany location in the world in </p>
                     <div>Hello World </div>
@@ -36,24 +43,24 @@ const Home = () => {
 
                 </div>
                 <div className="weather-details">
-                    {(!weatherData || !weatherData.location) ? ( <p style={{display:'flex', alignItems:'center', justifyContent:'center', width:'100%', height:'100%'}}>Loading.....</p>): (
+                    {(!weatherData || !weatherData.location) ? (<p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%' }}>Loading.....</p>) : (
                         <>
-                        <div className="location">  {weatherData.location.name}, {weatherData.location.region}, {weatherData.location.country}</div>
-                    <div className="data">
-                        <div>
-                            <img src={weatherData.current.weather_icons} alt="" />
-                            <p>{weatherData.current.weather_descriptions[0]}</p>
-                        </div>
-                        <h2 className="temprature">{weatherData.current.temperature}°C</h2>
-                        <div>
-                            <p>Wind: {weatherData.current.wind_speed} km/h</p>
-                            <p>Precip: {weatherData.current.precip}</p>
-                            <p>Pressure: {weatherData.current.pressure}</p>
-                        </div>
-                    </div>
+                            <div className="location">  {weatherData.location.name}, {weatherData.location.region}, {weatherData.location.country}</div>
+                            <div className="data">
+                                <div>
+                                    <img src={weatherData.current.weather_icons} alt="" />
+                                    <p>{weatherData.current.weather_descriptions[0]}</p>
+                                </div>
+                                <h2 className="temprature">{weatherData.current.temperature}°C</h2>
+                                <div>
+                                    <p>Wind: {weatherData.current.wind_speed} km/h</p>
+                                    <p>Precip: {weatherData.current.precip}</p>
+                                    <p>Pressure: {weatherData.current.pressure}</p>
+                                </div>
+                            </div>
                         </>
                     )}
-                    
+
 
                 </div>
             </div>
